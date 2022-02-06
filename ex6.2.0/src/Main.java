31540815c07056f132c585fdf9c11cdde83c2bf9
zagaynov-andrew <zagaynov.drew@gmail.com>
2022-02-06 15:26:26 +0300

    Add : symmetric difference v2.0

--- ex6.2.0/src/Main.java
+++ ex6.2.0/src/Main.java
@@ -5,20 +5,27 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Set<Integer> num1 = new HashSet<>();
+        num1.add(1);
+        num1.add(2);
+        num1.add(3);
+
+        HashSet<Integer> num2 = new HashSet<>();
+        num2.add(0);
+        num2.add(1);
+        num2.add(2);
+
+        Set<Integer> result = symmetricDifference(num1,num2);
+        System.out.println(result);
     }
 
     public static <T> Set<T> symmetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
 
-        Set<T> result = new HashSet<T>();
+        Set<T> result = new HashSet<>();
 
-        for (T value1 : set1) {
-            if (!set2.contains(value1))
-                result.add(value1);
-        }
-        for (T value2 : set2) {
-            if (!set1.contains(value2))
-                result.add(value2);
-        }
+        set1.forEach(elem -> { if (!set2.contains(elem)) result.add(elem); });
+        set2.forEach(elem -> { if (!set1.contains(elem)) result.add(elem); });
+
         return result;
     }
 }
